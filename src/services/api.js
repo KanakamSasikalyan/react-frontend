b66d53b6f9bb398dc29f95f3edde83062c8ebc0b
KanakamSasikalyan <kanakamsasikalyan1@gmail.com>
2025-04-15 22:39:13 +0530

    modified frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-const API_BASE_URL = 'https://fashion-studio-ai.onrender.com/api/designs'; // Update with your Render backend URL
+const API_BASE_URL = 'https://fashion-studio-ai.onrender.com';
+//const API_BASE_URL='';
 
 export const generateDesign = async (prompt, style) => {
     try {
-        const response = await axios.post(`${API_BASE_URL}/${encodeURIComponent(prompt)}?style=${style}`, null, {
+        const response = await axios.post(`${API_BASE_URL}/api/designs/${encodeURIComponent(prompt)}?style=${style}`, null, {
             responseType: 'arraybuffer',
             headers: {
                 'Content-Type': 'application/json',
