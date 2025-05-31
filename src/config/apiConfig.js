02c65071b4277832c19f157cc4ff6daf6b7abc2e
KanakamSasiKalyan <kanakamsasikalyan1@gmail.com>
2025-05-31 23:45:54 +0530

    updated frontend api

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 // // Added dynamic base URL configuration
 const API_BASE_URL = process.env.NODE_ENV === 'development' 
   ? 'http://localhost:8080' 
-  : 'https://fashion-studio-ai-zbt4.onrender.com';
+  : 'fashion-studio-ai-backend-b5hpfgcxagg3cpfn.centralus-01.azurewebsites.net';
 
 export default API_BASE_URL;
