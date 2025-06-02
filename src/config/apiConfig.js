f78930ad9650dc6164525aa0c8178613df20d75a
KanakamSasikalyan <kanakamsasikalyan1@gmail.com>
2025-06-03 01:16:04 +0530

    updated backend url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 // // Added dynamic base URL configuration
 const API_BASE_URL = process.env.NODE_ENV === 'development' 
   ? 'http://localhost:8080' 
-  : 'backend-fashion-studio-ai-f7e8a8cjepcfcge2.centralus-01.azurewebsites.net';
+  : 'https://backend-fashion-studio-ai-f7e8a8cjepcfcge2.centralus-01.azurewebsites.net';
 
 export default API_BASE_URL;
