d372dfe5f25ba2c584dbc95b4513a5338050a630
KanakamSasikalyan <kanakamsasikalyan1@gmail.com>
2025-08-08 01:21:56 +0530

    new-frontend@deployment

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 // // Added dynamic base URL configuration
 const API_BASE_URL = process.env.NODE_ENV === 'development' 
   ? 'http://localhost:8081' 
-  : 'https://ubc-fashion-studio-ai-backend-container-gbhxfzafhxddd8bx.centralus-01.azurewebsites.net';
+  : 'https://ubc-backend-fashion-studio-ai-server-btfmcahmf0b7ayfe.canadacentral-01.azurewebsites.net';
 
 export default API_BASE_URL;
